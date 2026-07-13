c539ce04f39ab6ae225729b7b4daae1bb505a550
paturrr <ahmad.fathurrohman26@gmail.com>
2026-07-13 21:15:29 +0700

    chore: bump version to 1.0.3 for english translation update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spolyrics',
-    version='1.0.2',
+    version='1.0.3',
     description='A minimalist, borderless, zero-delay synced lyrics miniplayer for Spotify on Windows.',
     author='SpoLyrics Team',
     py_modules=['main'],
