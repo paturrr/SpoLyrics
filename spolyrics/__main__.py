83fc46b93a37078ccb66397e0dbbcb15ec25df3a
paturrr <ahmad.fathurrohman26@gmail.com>
2026-07-23 09:04:23 +0700

    chore: bump version to 1.4.2 and add release notes

--- spolyrics/__main__.py
+++ spolyrics/__main__.py
@@ -25,7 +25,7 @@ from .assets import ICON_B64
 from tkinter import messagebox
 from winsdk.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
 
-CURRENT_VERSION = "1.4.1"
+CURRENT_VERSION = "1.4.2"
 CONFIG_PATH = os.path.join(os.environ.get("APPDATA", ""), "SpoLyrics", "config.json")
 APP_DIR = os.path.join(os.environ.get("APPDATA", ""), "SpoLyrics")
 os.makedirs(APP_DIR, exist_ok=True)
